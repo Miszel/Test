08dd66ea655636b4e0834661305b53f230cc9631
Karolina.Kowalik <kowalik.karolina@gmail.com>
2018-04-11 13:35:27 +0200

    third commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,10 +2,22 @@ package com.company;
 
 public class Main {
 
+ static int[] prices = {9,1,1};
+
+    public static float calculateAverage (int prices[]){
+        int sum = 0;
+        for(int i=0;i<prices.length;i++){
+        sum += prices[i];
+        }
+        float average = (float)sum/prices.length;
+        return average;
+    }
+
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
+       // float[] prices = {4, 7};
+	// write your code he
+        //Main main = new Main();
+                //System.out.println(main.calculateAverage(prices[0],prices[1]));
+        System.out.println(calculateAverage(prices));
     }
 }
--- src/com/company/exercise12.java
+++ src/com/company/exercise12.java
@@ -1,4 +1,40 @@
 package com.company;
 
+import static java.lang.Integer.*;
+
 public class exercise12 {
+
+public static int[] changeType() {
+    String formInput = "1;10";
+    String[] result = formInput.split(";");
+    int[] tabInt = new int[result.length];
+
+    for (int x=0; x<result.length; x++) {
+        tabInt[x] = Integer.valueOf(result[x]);
+
+        System.out.println(tabInt[x]);
+    }
+return tabInt;
+     }
+
+    public static float calculateAverage (int[] prices){
+
+
+                int sum = 0;
+        for (int price : prices) {
+            sum += price;
+        }
+        return (float)sum/prices.length;
+    }
+
+
+    public static void main(String[] args) {
+        //for (int x: result)
+       // System.out.println(x);
+        int[] tab = changeType();
+        float average =calculateAverage(tab);
+        System.out.println("srednia z tablicy to:" + average);
+    }
+
+
 }
