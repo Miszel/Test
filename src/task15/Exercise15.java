afcffde1e0940080cc9ff11a85b2120a00919333
karolina <kowalik.karolina@gmail.com>
2018-04-13 09:16:09 +0200

    exercise15

---
+++ src/task15/Exercise15.java
@@ -0,0 +1,40 @@
+package task15;
+
+public class Exercise15 {
+
+    public static Product[] changeType() {
+
+        String formInput = "10;2;1.5";
+        String[] result = formInput.split(";");
+        Product[] products = new Product[result.length];
+
+        for (int x=0; x<result.length; x++) {
+
+            Product product = new Product(Float.valueOf(result[x]), 0.5f);
+            System.out.println("netto product price:" + product.price);
+            System.out.println("tax product :" + product.tax);
+            products[x] = product;
+        }
+        return products;
+    }
+
+
+    public static float calculateAverage (Product[] tab){
+
+        float sum = 0;
+        for (int i=0;i<tab.length;i++) {
+            float gross = tab[i].calculateGrossPrice();
+            sum+=gross;
+        }
+        return (float)sum/tab.length;
+
+    }
+
+    public static void main(String[] args) {
+
+        Product[] table = changeType();
+        float average =calculateAverage(table);
+        System.out.println("average of all products:" + average);
+    }
+
+}
--- src/task15/Product.java
+++ src/task15/Product.java
@@ -1,15 +1,22 @@
-package com.company;
+package task15;
 
 public class Product {
     float price;
     float tax;
 
-      float calculateGrossPrice(){
+
+
+    Product(float price, float tax) {
+        this.price = price;
+        this.tax = tax;
+    }
+
+
+    float calculateGrossPrice(){
 
         float gross = (tax*price)+price;
         System.out.println("brutto product price:" + gross);
         return gross;
     }
-
 
 }
