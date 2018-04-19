0af0ace1ddb015e43d4528e1e43268046b120824
Karolina.Kowalik <kowalik.karolina@gmail.com>
2018-04-19 12:41:16 +0200

    cwiczenie 25

---
+++ src/task25/Product.java
@@ -0,0 +1,47 @@
+package task25;
+
+public class Product {
+    private float price;
+    private float tax;
+
+    Product(float price, float tax) {
+        this.price = price;
+        this.tax = tax;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+    public float getTax() {
+        return tax;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public void setTax(float tax) {
+        this.tax = tax;
+    }
+
+
+    float calculateGrossPrice(){
+
+        float gross = (tax*price)+price;
+        //System.out.println("brutto product price:" + gross);
+        return gross;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Product with price " + getPrice() + " and tax of " + getTax();
+    }
+
+}
+
+
+
+
+
+
